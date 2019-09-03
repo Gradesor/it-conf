709b0d1f7ded400e2e90f49df8017ab68dc2bfb8
Gradesor <heruvim694@gmail.com>
2019-09-03 08:03:22 +0500

    Почти всё доверстал, осталось адаптировать подвал, сделал вроде красивую поблочную прокрутку, немного с ней повозился, но получилось вроде не плохо, осталось самая малость

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,22 @@
 $(document).ready(function () {	
-	
+	$.scrollify({
+		section: ".slide",
+		sectionName: "section-name",
+		easing: "easeOutExpo",
+		scrollSpeed: 1100,
+		offset: 10,
+		scrollbars: false,
+		standardScrollElements: "",
+		setHeights: true,
+		overflowScroll: true,
+		updateHash: true,
+		touchScroll: true
+	});
+	$('.map')
+		.click(function () {
+			$(this).find('iframe').addClass('clicked')
+		})
+		.mouseleave(function () {
+			$(this).find('iframe').removeClass('clicked')
+		});
 })
